61c48ace3388c05fa90316559eb6225da5355586
Avijit Dutta <avijitdutta1230000@gmail.com>
2026-05-19 13:29:38 +0530

    Added room system, online users count and language selector

--- server/index.js
+++ server/index.js
@@ -11,18 +11,34 @@ const io = new Server(server, {
   cors: { origin: 'http://localhost:3000' }
 });
 
+const rooms = {};
+
 io.on('connection', (socket) => {
   console.log('User connected:', socket.id);
 
-  socket.on('code-change', (code) => {
-    socket.broadcast.emit('code-update', code);
+  socket.on('join-room', ({ roomId, username }) => {
+    socket.join(roomId);
+
+    if (!rooms[roomId]) rooms[roomId] = [];
+    rooms[roomId].push({ id: socket.id, username });
+
+    io.to(roomId).emit('users-update', rooms[roomId]);
+    console.log(`${username} joined room ${roomId}`);
   });
 
-  socket.on('language-change', (language) => {
-    socket.broadcast.emit('language-update', language);
+  socket.on('code-change', ({ roomId, code }) => {
+    socket.to(roomId).emit('code-update', code);
+  });
+
+  socket.on('language-change', ({ roomId, language }) => {
+    socket.to(roomId).emit('language-update', language);
   });
 
   socket.on('disconnect', () => {
+    for (const roomId in rooms) {
+      rooms[roomId] = rooms[roomId].filter(u => u.id !== socket.id);
+      io.to(roomId).emit('users-update', rooms[roomId]);
+    }
     console.log('User disconnected:', socket.id);
   });
 });
