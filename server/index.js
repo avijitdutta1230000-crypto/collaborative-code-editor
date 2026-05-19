4e03ed4e978429ccb296dd1117d0b0a107ba6e35
Avijit Dutta <avijitdutta1230000@gmail.com>
2026-05-19 14:00:57 +0530

    Added room system,run code ,download button and online user

--- server/index.js
+++ server/index.js
@@ -5,12 +5,36 @@ const cors = require('cors');
 
 const app = express();
 app.use(cors());
+app.use(express.json());
 
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: 'http://localhost:3000' }
 });
 
+// Run Code Route
+app.post('/run', async (req, res) => {
+  const { code, language } = req.body;
+  try {
+    const fetch = (await import('node-fetch')).default;
+    const response = await fetch('https://emkc.org/api/v2/piston/execute', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        language: language,
+        version: '*',
+        files: [{ content: code }],
+      }),
+    });
+    const data = await response.json();
+    res.json(data);
+  } catch (err) {
+    console.log('Error:', err);
+    res.json({ run: { stdout: '', stderr: 'Error running code!' } });
+  }
+});
+
+// Socket.io
 const rooms = {};
 
 io.on('connection', (socket) => {
@@ -18,10 +42,8 @@ io.on('connection', (socket) => {
 
   socket.on('join-room', ({ roomId, username }) => {
     socket.join(roomId);
-
     if (!rooms[roomId]) rooms[roomId] = [];
     rooms[roomId].push({ id: socket.id, username });
-
     io.to(roomId).emit('users-update', rooms[roomId]);
     console.log(`${username} joined room ${roomId}`);
   });
