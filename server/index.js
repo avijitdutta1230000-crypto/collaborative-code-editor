06742c8dc492fe59e76c829c6b1087d98e433072
Avijit Dutta <avijitdutta1230000@gmail.com>
2026-05-20 13:07:47 +0530

    Added server health check route

--- server/index.js
+++ server/index.js
@@ -6,7 +6,9 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.get('/', (req, res) => {
+  res.send('🚀 Collaborative Code Editor Server is Running!');
+});
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: { origin: 'http://localhost:3000' }
